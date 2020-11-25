71362be04a7fa8fd72727c95b942119d966bfd45
Dan Nguyen <dansonguyen@gmail.com>
2020-11-24 19:02:13 -0600

    chicago cdph inspections

---
+++ TODOS.md
@@ -0,0 +1,7 @@
+# TODOS
+
+
+**meta.yaml schema**
+
+- should sources.download be changed to sources.original?
+- should :files: be changed to :stashes/stash:
---
+++ cli/__main__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def stash(slug):
+    """
+    TK pseudo-code
+
+    slug = 'chicago-environmental-inspections'
+
+    source_url = sources['download']
+    dest_dir = Path('data', slug)
+
+    mkdir -p dest_dir
+    dest_url = get_default_stash_filename
+    # curl -o dest_dir/get_default_stash_filename  dest_url
+    """
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
